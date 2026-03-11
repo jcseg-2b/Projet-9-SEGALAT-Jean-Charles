2a41ffda2cfaaf83bc78533e441c08e8c6ec2582
jcseg-2b <jcmailprolinkedin@gmail.com>
2026-03-11 11:21:31 +0100

    Correction du bug lié aux filtres par catégorie

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,22 +13,24 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((!type ? data?.events : data?.events) || []).filter(
-    (event, index) => {
-      if (
-        (currentPage - 1) * PER_PAGE <= index &&
-        PER_PAGE * currentPage > index
-      ) {
-        return true;
-      }
-      return false;
-    },
-  );
+  const filteredByType =
+    (!type
+      ? data?.events
+      : data?.events.filter((event) => event.type === type)) || [];
+  const filteredEvents = filteredByType.filter((event, index) => {
+    if (
+      (currentPage - 1) * PER_PAGE <= index &&
+      PER_PAGE * currentPage > index
+    ) {
+      return true;
+    }
+    return false;
+  });
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  const pageNumber = Math.ceil((filteredByType?.length || 0) / PER_PAGE);
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
